301c995fbb871cc8c375e978be1f2f712f10e12b
ogunnowoomotayo <81049138+ogunnowoomotayo@users.noreply.github.com>
2023-11-15 13:22:20 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+
+meun.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+}
+)
